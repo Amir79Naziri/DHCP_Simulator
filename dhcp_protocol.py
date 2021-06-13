3c802f8a295e458de8807b2142c4f8fc7466ee86
Amir79Naziri <naziriamirreza@gmail.com>
2021-06-14 01:46:10 +0430

    "feat: complete dhcp protocol decoder"

--- dhcp_protocol.py
+++ dhcp_protocol.py
@@ -1,7 +1,7 @@
 from uuid import getnode as get_mac
 import random
 import struct
-import socket
+
 
 
 class HARDCODES:
@@ -12,7 +12,7 @@ class HARDCODES:
     HOPS = b'\x00'
     SEC_ELAPSED = b'\x00\x00'
     BOOTP_FLAGS = b'\x00\x00'
-    MAC_ADDR_PADDING = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    MAC_ADDR_PADDING = b'\x00' * 10
     SNAME = b'\x00' * 64
     FILE = b'\x00' * 128
     DEFAULT_IP = b'\x00\x00\x00\x00'
@@ -65,14 +65,12 @@ def DHCP_discover_encode(ID):
     # OPTIONS
 
     query += b'\x35\x01\x01'  # Option: (t=53,l=1) DHCP Message Type = DHCP Discover
-    query += b'\x33\x06' + MAC  # Option: (t=51, l=6) Client identifier
-    query += b'\x37\x03\x03\x01\x06'  # Option: (t=55,l=3) Parameter Request List
     query += HARDCODES.END_OPTION
 
     return query
 
 
-def DHCP_offer_encode(ID, suggested_ip):
+def DHCP_offer_encode(ID, suggested_ip, server_ip, lease_time):
     query = HARDCODES.REPLY
     query += HARDCODES.ETHERNET
     query += HARDCODES.HARDWARE_ADDR_LEN
@@ -82,7 +80,7 @@ def DHCP_offer_encode(ID, suggested_ip):
     query += HARDCODES.BOOTP_FLAGS
     query += HARDCODES.DEFAULT_IP
     query += ip_addr_to_byte(suggested_ip)
-    query += ip_addr_to_byte(socket.gethostbyname(socket.gethostname()))
+    query += ip_addr_to_byte(server_ip)
     query += HARDCODES.DEFAULT_IP
     MAC = system_mac_addr()
     query += MAC
@@ -93,8 +91,7 @@ def DHCP_offer_encode(ID, suggested_ip):
     # OPTIONS
 
     query += b'\x35\x01\x02'  # Option: (t=53,l=1) DHCP Message Type = DHCP Offer
-    query += b'\x33\x02\x0E\x10'  # Option: (t=51,l=2)  lease time
-    query += b'\x36\x04' + ip_addr_to_byte(socket.gethostbyname(socket.gethostname()))  # Option: (t=54,l=4)
+    query += b'\x33\x04' + struct.pack('!L', lease_time)  # Option: (t=51,l=2)  lease time
     query += HARDCODES.END_OPTION
 
     return query
@@ -121,14 +118,12 @@ def DHCP_request_encode(ID, requested_ip, server_ip):
     # OPTIONS
 
     query += b'\x35\x01\x03'  # Option: (t=53,l=1) DHCP Message Type = DHCP Request
-    query += b'\x32\x04' + ip_addr_to_byte(requested_ip)   # Option: (t=50,l=4) requested IP
-    query += b'\x36\x04' + ip_addr_to_byte(server_ip)  # Option: (t=54,l=4) server IP
     query += HARDCODES.END_OPTION
 
     return query
 
 
-def DHCP_ack_encode(ID, allocated_ip, lease_time_bytes):
+def DHCP_ack_encode(ID, allocated_ip, server_ip, lease_time):
     query = HARDCODES.REPLY
     query += HARDCODES.ETHERNET
     query += HARDCODES.HARDWARE_ADDR_LEN
@@ -138,7 +133,7 @@ def DHCP_ack_encode(ID, allocated_ip, lease_time_bytes):
     query += HARDCODES.BOOTP_FLAGS
     query += HARDCODES.DEFAULT_IP
     query += ip_addr_to_byte(allocated_ip)
-    query += ip_addr_to_byte(socket.gethostbyname(socket.gethostname()))
+    query += ip_addr_to_byte(server_ip)
     query += HARDCODES.DEFAULT_IP
     MAC = system_mac_addr()
     query += MAC
@@ -149,21 +144,100 @@ def DHCP_ack_encode(ID, allocated_ip, lease_time_bytes):
     # OPTIONS
 
     query += b'\x35\x01\x05'  # Option: (t=53,l=1) DHCP Message Type = DHCP ack
-    query += b'\x33\x02' + lease_time_bytes  # Option: (t=51,l=2)  lease time
-    query += ip_addr_to_byte(socket.gethostbyname(socket.gethostname()))
+    query += b'\x33\x04' + struct.pack('!L', lease_time)  # Option: (t=51,l=2)  lease time
     query += HARDCODES.END_OPTION
 
     return query
 
 
-def DHCP_offer_decode(data, ID):
-    if data[4:8] == ID:
-        offerIP = '.'.join(map(lambda x: str(x), data[16:20]))
-        nextServerIP = '.'.join(map(lambda x: str(x), data[20:24]))
-        DHCPServerIdentifier = '.'.join(map(lambda x: str(x), data[245:249]))
-        leaseTime = str(struct.unpack('!L', data[251:255])[0])
+def DHCP_decode(data):
+    decoded_data = dict()
+
+    pivot = 0
+    decoded_data['OP'] = data[pivot:pivot + 1]
+    pivot += 1
+    decoded_data['HW_TYPE'] = data[pivot:pivot + 1]
+    pivot += 1
+    decoded_data['HW_LEN'] = data[pivot:pivot + 1]
+    pivot += 1
+    decoded_data['HOPS'] = data[pivot:pivot + 1]
+    pivot += 1
+    decoded_data['XID'] = data[pivot:pivot + 4]
+    pivot += 4
+    decoded_data['SECS'] = data[pivot:pivot + 2]
+    pivot += 2
+    decoded_data['FLAGS'] = data[pivot:pivot + 2]
+    pivot += 2
+    decoded_data['CI_ADDR'] = '.'.join(map(lambda x: str(x), data[pivot:pivot + 4]))
+    pivot += 4
+    decoded_data['YI_ADDR'] = '.'.join(map(lambda x: str(x), data[pivot:pivot + 4]))
+    pivot += 4
+    decoded_data['SI_ADDR'] = '.'.join(map(lambda x: str(x), data[pivot:pivot + 4]))
+    pivot += 4
+    decoded_data['GI_ADDR'] = '.'.join(map(lambda x: str(x), data[pivot:pivot + 4]))
+    pivot += 4
+    MAC = data[pivot:pivot + 16].hex()
+    decoded_data['CH_ADDR'] = '.'.join(MAC[i:i+2] for i in range(0, 12, 2))
+    pivot += 16
+    decoded_data['S_NAME'] = data[pivot:pivot + 64]
+    pivot += 64
+    decoded_data['FILE'] = data[pivot:pivot + 128]
+    pivot += 128
 
 
-if __name__ == '__main__':
-    print(socket.gethostbyname(socket.gethostname()))
-    print(ip_addr_to_byte(socket.gethostbyname(socket.gethostname())))
+    # OPTIONS
+
+    pivot_copy = pivot
+
+    while True:
+        T = data[pivot_copy]
+        pivot_copy += 1
+        L = data[pivot_copy]
+        pivot_copy += 1
+        if T == 53:
+            code = data[pivot_copy]
+            if code == 1:
+                M_TYPE = 'DISCOVER'
+            elif code == 2:
+                M_TYPE = 'OFFER'
+            elif code == 3:
+                M_TYPE = 'REQUEST'
+            elif code == 5:
+                M_TYPE = 'ACK'
+            else:
+                M_TYPE = 'UNKNOWN'
+            break
+
+        elif T == 255:
+            M_TYPE = 'UNKNOWN'
+            break
+        else:
+            pivot_copy += L
+
+    if M_TYPE == 'UNKNOWN':
+        return dict()
+
+    decoded_data['M_TYPE'] = M_TYPE
+
+    if M_TYPE == 'OFFER' or M_TYPE == 'ACK':
+        pivot_copy = pivot
+
+        while True:
+            T = data[pivot_copy]
+            pivot_copy += 1
+            L = data[pivot_copy]
+            pivot_copy += 1
+            if T == 51:
+                decoded_data['lease_time'] = int(struct.unpack('!L', data[pivot_copy:pivot_copy + L])[0])
+                break
+            elif T == 255:
+                decoded_data['lease_time'] = -1
+                break
+            else:
+                pivot_copy += L
+    else:
+        decoded_data['lease_time'] = -1
+
+    return decoded_data
+
+
