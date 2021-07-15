a70aa41df439f9120b153701d463125477210cac
Amir79Naziri <naziriamirreza@gmail.com>
2021-07-15 21:15:10 +0430

    "feat: Add offer and ack functions"

--- server.py
+++ server.py
@@ -1,7 +1,23 @@
 import socket
-import threading
-import dhcp_protocol
+from dhcp_protocol import DHCP_offer_encode, DHCP_ack_encode, DHCP_decode
 from ip_pool import Pool
+
+SERVER_IP = None
+pool = None
+
+
+def offer(serverSocket, ID, mac_addr, device_name):
+    offered_ip = pool.offer_ip(mac_addr, device_name)
+    print(offered_ip)
+    s_query = DHCP_offer_encode(ID, offered_ip, SERVER_IP, pool.lease_time(), mac_addr)
+    serverSocket.sendto(s_query, ('<broadcast>', 9090))
+
+
+def ack(serverSocket, ID, mac_addr, device_name):
+    allocated_ip = pool.allocate_ip(mac_addr, device_name)
+    print(allocated_ip)
+    s_query = DHCP_ack_encode(ID, allocated_ip, SERVER_IP, pool.lease_time(), mac_addr)
+    serverSocket.sendto(s_query, ('<broadcast>', 9090))
 
 
 def start_server():
@@ -9,17 +25,19 @@ def start_server():
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as serverSocket:
             serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-            address = (socket.gethostbyname(socket.gethostname()), 8080)
+            address = (SERVER_IP, 8080)
             serverSocket.bind(address)
 
-            pool = Pool()
             print('Server started...')
 
             while True:
-                data, addr = serverSocket.recvfrom(1024)
-                print(data)
-                print(addr)
-                serverSocket.sendto(b'OK', ('<broadcast>', 9090))
+                r_query, _ = serverSocket.recvfrom(1024)
+                data = DHCP_decode(r_query)
+                if data['OP'] == 1 and data['M_TYPE'] == 'DISCOVER':
+                    pass
+                elif data['OP'] == 1 and data['M_TYPE'] == 'REQUEST':
+                    pass
+
 
     except ConnectionError as msg:
         print(msg)
@@ -28,4 +46,6 @@ def start_server():
 
 
 if __name__ == '__main__':
+    SERVER_IP = socket.gethostbyname(socket.gethostname())
+    pool = Pool()
     start_server()
